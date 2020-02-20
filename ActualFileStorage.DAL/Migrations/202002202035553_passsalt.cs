1f9d53192f88ab78c114d0bf96dfdf3487af3916
PortalArm <evgenii_kim2010@mail.ru>
2020-02-21 02:48:51 +0600

    Add migration (salt)

---
+++ ActualFileStorage.DAL/Migrations/202002202035553_passsalt.cs
@@ -0,0 +1,18 @@
+﻿namespace ActualFileStorage.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class passsalt : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Users", "Salt", c => c.String(nullable: false, maxLength: 64));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Users", "Salt");
+        }
+    }
+}
--- ActualFileStorage.DAL/Migrations/Configuration.cs
+++ ActualFileStorage.DAL/Migrations/Configuration.cs
@@ -26,11 +26,11 @@
         }
 
         private User[] Users = {
-            new User(){ Login="user1", BirthDate = DateTime.Now, Email = "haha@gma7il.com", PassHash="335", FirstName = "Ye52ll" },
-            new User(){ Login="user2", BirthDate = DateTime.Now, Email = "ha2ha@gm4ail.com", PassHash="357", FirstName = "Yel1l" },
-            new User(){ Login="user3", BirthDate = DateTime.Now, Email = "hah87a@gmail.7com", PassHash="345", FirstName = "Y5e4ll" },
-            new User(){ Login="user111", BirthDate = DateTime.Now, Email = "h61aha@gma3il.com", PassHash="385", FirstName = "Ye5ll" },
-            new User(){ Login="user66", BirthDate = DateTime.Now, Email = "ha2ha@g8mail.com", PassHash="315", FirstName = "Ye5l5l" },
+            new User(){ Login="user1", BirthDate = DateTime.Now, Email = "haha@gma7il.com", PassHash="335", FirstName = "Ye52ll", Salt="34s089yuh4d8i9gjdriogjxdr0oi9jgxr9d4jh09sejh55" },
+            new User(){ Login="user2", BirthDate = DateTime.Now, Email = "ha2ha@gm4ail.com", PassHash="357", FirstName = "Yel1l", Salt="se094hjydxopriy09se4ujhgjdrxf9yjxd-0hjk490djyh" },
+            new User(){ Login="user3", BirthDate = DateTime.Now, Email = "hah87a@gmail.7com", PassHash="345", FirstName = "Y5e4ll", Salt="e409uyrdj0x9gdiopytues40xityxeds0hjgriojhgtjky" },
+            new User(){ Login="user111", BirthDate = DateTime.Now, Email = "h61aha@gma3il.com", PassHash="385", FirstName = "Ye5ll",Salt="09s34hjtosdxedj8othseo58kxy908xdrhty0ooesjelhm"},
+            new User(){ Login="user66", BirthDate = DateTime.Now, Email = "ha2ha@g8mail.com", PassHash="315", FirstName = "Ye5l5l", Salt="4e89tyhedoinxgiusvbr2ao9tj4938whg09s4euyt4o8he" },
         };
 
         private Folder[] GetFolders()
