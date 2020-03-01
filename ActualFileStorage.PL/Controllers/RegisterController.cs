4b189b4a8db3b0c9458f467689661e51fff665b4
PortalArm <evgenii_kim2010@mail.ru>
2020-03-02 01:14:08 +0600

    Add registration procedure

--- ActualFileStorage.PL/Controllers/RegisterController.cs
+++ ActualFileStorage.PL/Controllers/RegisterController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ActualFileStorage.BLL.Services;
+using ActualFileStorage.PL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +10,47 @@ namespace ActualFileStorage.PL.Controllers
 {
     public class RegisterController : Controller
     {
-
+        RegistrationService _service;
+        public RegisterController(RegistrationService service)
+        {
+            _service = service;
+        }
         // GET: Register
         public ActionResult Index()
         {
+
             return View();
         }
+
+        public ActionResult RegForm()
+        {
+            return PartialView();
+        }
+        public JsonResult IsLoginPresent(string login)
+        {
+            if (_service.LoginExists(login))
+                return Json("This login is already occupied.", JsonRequestBehavior.AllowGet);
+
+            return Json(true, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult RegAjaxForm(RegistrationUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return new JsonResult() { ContentType = "application/json", Data = new { status = "error" } };
+
+            DAL.Models.User u = new DAL.Models.User() {
+                BirthDate = model.BirthDate,
+                Email = model.Email,
+                FirstName = model.FirstName,
+                Login = model.Login,
+                SecondName = model.SecondName
+            };
+            _service.Register(u, model.Password);
+            return new JsonResult() { ContentType = "application/json", Data = new { status = "ok" } };
+            //RedirectToAction("Index", "Home");
+        }
+
     }
 }
