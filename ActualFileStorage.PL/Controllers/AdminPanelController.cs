54f4e1113044f2aff53cef660b8f8958ea00789c
PortalArm <evgenii_kim2010@mail.ru>
2020-03-02 01:13:34 +0600

    Add admin panel prototype

---
+++ ActualFileStorage.PL/Controllers/AdminPanelController.cs
@@ -0,0 +1,35 @@
+﻿using ActualFileStorage.DAL.Models;
+using ActualFileStorage.DAL.UOW;
+using ActualFileStorage.PL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ActualFileStorage.PL.Controllers
+{
+    public class AdminPanelController : Controller
+    {
+        IUnitOfWork _uow;
+        public AdminPanelController(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+        public ActionResult Index()
+        {
+            var users = _uow.GetRepo<User>();
+            var userInfos = users.GetAll().Select(u => new UserViewModel() {
+                BirthDate = u.BirthDate,
+                Email = u.Email,
+                FirstName = u.FirstName,
+                Id = u.Id,
+                Login = u.Login,
+                RootFolderAccess = u.Folder.Visibility,
+                SecondName = u.SecondName
+            });
+
+            return View(userInfos);
+        }
+    }
+}
---
+++ ActualFileStorage.PL/Models/UserViewModel.cs
@@ -0,0 +1,21 @@
+﻿using ActualFileStorage.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ActualFileStorage.PL.Models
+{
+    public class UserViewModel
+    {
+        public int Id { get; set; } 
+        public string Login { get; set; }
+        public string FirstName { get; set; } 
+        public string SecondName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+        public FileAccess RootFolderAccess { get; set; }
+
+    }
+}
