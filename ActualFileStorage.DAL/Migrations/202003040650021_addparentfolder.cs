44ea3ebff144c5a7bbba73adb575c4aa73377a93
PortalArm <evgenii_kim2010@mail.ru>
2020-03-05 01:35:02 +0600

    Change migration behaviour

---
+++ ActualFileStorage.DAL/Migrations/202003040650021_addparentfolder.cs
@@ -0,0 +1,22 @@
+﻿namespace ActualFileStorage.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addparentfolder : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Folders", "ParentFolder_Id", c => c.Int());
+            CreateIndex("dbo.Folders", "ParentFolder_Id");
+            AddForeignKey("dbo.Folders", "ParentFolder_Id", "dbo.Folders", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Folders", "ParentFolder_Id", "dbo.Folders");
+            DropIndex("dbo.Folders", new[] { "ParentFolder_Id" });
+            DropColumn("dbo.Folders", "ParentFolder_Id");
+        }
+    }
+}
