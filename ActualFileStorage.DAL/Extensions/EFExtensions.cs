f15b6fe70feff787e7c91175479f7534f3e86aa5
PortalArm <evgenii_kim2010@mail.ru>
2020-03-05 01:34:36 +0600

    Change how extension method work

--- ActualFileStorage.DAL/Extensions/EFExtensions.cs
+++ ActualFileStorage.DAL/Extensions/EFExtensions.cs
@@ -12,10 +12,30 @@ namespace ActualFileStorage.DAL.Extensions
     {
         public static IEnumerable<T> FindPredicate<T>(this DbSet<T> dbSet, Expression<Func<T, bool>> predicate) where T : class
         {
-            var local = dbSet.Local.Where(predicate.Compile());
-            return local.Any()
-                ? local
-                : dbSet.Where(predicate).ToArray();
+            var a = dbSet.Local;
+            var b = a.Where(predicate.Compile());
+            if (b.FirstOrDefault() != null)
+                return b.ToList();
+            var c = dbSet.Where(predicate);
+            var d = c.ToList();
+            return d;
+            //var c = b.ToList();
+            //return c;
+            //IEnumerable<T> local = null;
+            //if(dbSet.Local != null)
+            //    local = dbSet.Local.Where(predicate.Compile());
+            ////return local.Any()
+            ////    ? local
+            ////    : dbSet.Where(predicate).ToArray();
+            //if (local != null)
+            //    return local;
+            //var fromDb = dbSet.Where(predicate);
+            //if (fromDb.Any())
+            //    return fromDb.ToArray();
+            //return null;
+            //return local.Any()
+            //    ? local
+            //    : (dbSet.Where(predicate)).ToArray();
         }
     }
 }
