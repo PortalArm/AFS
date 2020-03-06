71fcb44ade806bd1bfdaf58b8bda335e82135568
PortalArm <evgenii_kim2010@mail.ru>
2020-03-07 01:45:21 +0600

    Add linq extension method helper

---
+++ ActualFileStorage.BLL/Extensions/LinqExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActualFileStorage.BLL.Extensions
+{
+    public static class LinqExtensions
+    {
+        public static IEnumerable<T> TakeWhileInclude<T>(this IEnumerable<T> list, Func<T, bool> predicate)
+        {
+            foreach (T item in list)
+            {
+                yield return item;
+                if (!predicate(item))
+                    yield break;
+            }
+        }
+    }
+}
