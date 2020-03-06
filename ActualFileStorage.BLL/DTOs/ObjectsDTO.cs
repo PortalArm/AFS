ad28342a9e25fb4fc61c8f9234b8bd7ab1afdf57
PortalArm <evgenii_kim2010@mail.ru>
2020-03-07 01:45:02 +0600

    Add more DTOs

---
+++ ActualFileStorage.BLL/DTOs/ObjectsDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActualFileStorage.BLL.DTOs
+{
+    public class ObjectsDTO
+    {
+        public IEnumerable<FileDTO> Files { get; set; }
+        public IEnumerable<FolderDTO> Folders { get; set; }
+        public IEnumerable<int?> History { get; set; }
+    }
+}
---
+++ ActualFileStorage.BLL/DTOs/UserDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActualFileStorage.BLL.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; } // req 
+        public string Login { get; set; } // req 
+        public string FirstName { get; set; } // req 
+        public string SecondName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Email { get; set; }
+
+    }
+}
