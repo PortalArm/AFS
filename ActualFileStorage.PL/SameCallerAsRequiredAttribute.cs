b24087d5ebff07f2b24f3cce7ca2c65530e1ab66
PortalArm <evgenii_kim2010@mail.ru>
2020-03-20 19:15:28 +0600

    Add custom attribute

---
+++ ActualFileStorage.PL/SameCallerAsRequiredAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Filters;
+
+namespace ActualFileStorage.PL
+{
+    public class SameCallerAsRequiredAttribute : ActionFilterAttribute, IAuthenticationFilter
+    {
+        public void OnAuthentication(AuthenticationContext filterContext)
+        {
+        }
+
+        public void OnAuthenticationChallenge(AuthenticationChallengeContext filterContext)
+        {
+            int? fromSessionId = (int?)filterContext.HttpContext.Session["userId"];
+            if (!filterContext.HttpContext.User.Identity.IsAuthenticated || !fromSessionId.HasValue)
+                filterContext.Result = new HttpUnauthorizedResult("Неавторизованным запрещено это действие");
+            var callerId = int.Parse(((ClaimsIdentity)filterContext.HttpContext.User.Identity).FindFirst(ClaimTypes.NameIdentifier).Value);
+            if (callerId != fromSessionId)
+                filterContext.Result = new HttpUnauthorizedResult("Данное действие разрешено только создателю");
+            //
+        }
+    }
+}
