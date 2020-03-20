b860b7c27e30771dff226f898d86936ea7d5fecb
PortalArm <evgenii_kim2010@mail.ru>
2020-03-20 19:10:38 +0600

    Change random passhash and salt to certain value Change extension to begin with a dot

--- ActualFileStorage.DAL/Migrations/Configuration.cs
+++ ActualFileStorage.DAL/Migrations/Configuration.cs
@@ -82,7 +82,7 @@ GO
         {
             File[] files = new File[count];
             for (int i = 0; i < count; ++i)
-                files[i] = new File() { Hash=(baseFolder.Id*16 + i+i).ToString(), Size = 64*i,  Name="file"+i, Ext = "txt", CreationTime = UC, Folder = baseFolder, Visibility = FileAccess.Public };
+                files[i] = new File() { Hash=(baseFolder.Id*16 + i+i).ToString(), Size = 64*i,  Name="file"+i, Ext = ".txt", CreationTime = UC, Folder = baseFolder, Visibility = FileAccess.Public };
             return files;
         }
 
@@ -102,7 +102,7 @@ GO
         private User[] Users = {
             new User(){ Login="user743", BirthDate = UC, Email = "haha@gma7il.com", PassHash="335", FirstName = "Ye52ll", Salt="34s089yuh4d8i9gjdriogjxdr0oi9jgxr9d4jh09sejh55" },
             new User(){ Login="user2", BirthDate = UC, Email = "ha2ha@gm4ail.com", PassHash="357", FirstName = "Yel1l", Salt="se094hjydxopriy09se4ujhgjdrxf9yjxd-0hjk490djyh" },
-            new User(){ Login="user3", BirthDate = UC, Email = "hah87a@gmail.7com", PassHash="345", FirstName = "Y5e4ll", Salt="e409uyrdj0x9gdiopytues40xityxeds0hjgriojhgtjky" },
+            new User(){ Login="user3", BirthDate = UC, Email = "hah87a@gmail.7com", PassHash="9a678c4f59769fe8cc0da3f3a024e64783d3d37566bad2b78bbb1cab00eea469", FirstName = "Y5e4ll", Salt="fzUBH5u4AwA0BH8yMz4I7J4sq0qHqigzvunQMkD5eIdisFFrVHrTzJ9HwlI0hPAm" },
             new User(){ Login="user111", BirthDate = UC, Email = "h61aha@gma3il.com", PassHash="385", FirstName = "Ye5ll",Salt="09s34hjtosdxedj8othseo58kxy908xdrhty0ooesjelhm"},
             new User(){ Login="user66", BirthDate = UC, Email = "ha2ha@g8mail.com", PassHash="315", FirstName = "Ye5l5l", Salt="4e89tyhedoinxgiusvbr2ao9tj4938whg09s4euyt4o8he" },
         };
