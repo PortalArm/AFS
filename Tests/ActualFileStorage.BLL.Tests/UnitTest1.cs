192555b0fa727157e0cbedb79987caada94952eb
PortalArm <evgenii_kim2010@mail.ru>
2020-03-20 19:03:22 +0600

    Add empty unit test projects

---
+++ Tests/ActualFileStorage.BLL.Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace ActualFileStorage.BLL.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TwoPlusThreeEqualFour()
+        {
+            int a = 2;
+            int b = 3;
+            int expected = 5;
+
+            int actual = a + b;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
