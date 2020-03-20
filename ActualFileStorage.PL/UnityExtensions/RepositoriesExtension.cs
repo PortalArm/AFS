a39cccaf877bb97746ace1155039a2c6a900165a
PortalArm <evgenii_kim2010@mail.ru>
2020-03-20 19:14:38 +0600

    Add Unity extensions

---
+++ ActualFileStorage.PL/UnityExtensions/RepositoriesExtension.cs
@@ -0,0 +1,21 @@
+﻿using ActualFileStorage.DAL.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Unity.Extension;
+using Unity;
+
+namespace ActualFileStorage.PL.UnityExtensions
+{
+    public class RepositoriesExtension : UnityContainerExtension
+    {
+        protected override void Initialize()
+        {
+            Container.RegisterType<IUserRepository, UserRepository>();
+            Container.RegisterType<IFolderRepository, FolderRepository>();
+            Container.RegisterType<IFileRepository, FileRepository>();
+            Container.RegisterType<IWebRoleRepository, WebRoleRepository>();
+        }
+    }
+}
---
+++ ActualFileStorage.PL/UnityExtensions/ServicesExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Unity.Extension;
+using Unity;
+using ActualFileStorage.BLL.Services.Interfaces;
+using ActualFileStorage.BLL.Services;
+
+namespace ActualFileStorage.PL.UnityExtensions
+{
+    public class ServicesExtension : UnityContainerExtension
+    {
+        protected override void Initialize()
+        {
+            //Container.RegisterType<>
+            Container.RegisterType<IAuthService, AuthService>();
+            Container.RegisterType<IAdminService, AdminService>();
+            Container.RegisterType<IProfileService, ProfileService>();
+            Container.RegisterType<IRegistrationService, RegistrationService>();
+        }
+    }
+}
